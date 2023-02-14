e1034fa308ff51c6edc529e0bf709da321ec32e7
eliam <eliamtraas@gmail.com>
2023-02-15 00:33:33 +0100

    sfdsdf

--- opdracht_1.hs
+++ opdracht_1.hs
@@ -7,3 +7,6 @@ faca n = n * faca ( n - 1 )
 facb :: Integer -> Integer 
 facb n | n == 0 = 1 
        | n /= 0 = n * facb (n-1) 
+
+addThree :: Int -> Int -> Int -> Int  
+addThree x y z = x + y + z 
