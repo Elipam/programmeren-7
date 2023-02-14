018ec623d8bad9c2f6f11c5188392021dcfc39db
eliam <eliamtraas@gmail.com>
2023-02-14 22:55:07 +0100

    message

--- opdracht_1.hs
+++ opdracht_1.hs
@@ -1,13 +1,9 @@
--- 1a
-factorial :: (Integral a) => a -> a  
-factorial 0 = 1  
-factorial n = n * factorial (n - 1)  
+-- 1a 
+faca :: Integer -> Integer 
+faca 0 = 1 
+faca n = n * faca ( n - 1 ) 
 
 -- 1b
-sayMe :: (Integral a) => a -> String  
-sayMe 1 = "One!"  
-sayMe 2 = "Two!"  
-sayMe 3 = "Three!"  
-sayMe 4 = "Four!"  
-sayMe 5 = "Five!"  
-sayMe x = "Not between 1 and 5"  
+facb :: Integer -> Integer 
+facb n | n == 0 = 1 
+       | n /= 0 = n * facb (n-1) 
