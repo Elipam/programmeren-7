834629ec6f817e0aa9ef35f66d66382ad0cb4c0b
eliam <eliamtraas@gmail.com>
2023-02-14 19:58:56 +0100

    alle opdrachten

--- opdracht_1.hs
+++ opdracht_1.hs
@@ -1,3 +1,13 @@
+-- 1a
 factorial :: (Integral a) => a -> a  
 factorial 0 = 1  
 factorial n = n * factorial (n - 1)  
+
+-- 1b
+sayMe :: (Integral a) => a -> String  
+sayMe 1 = "One!"  
+sayMe 2 = "Two!"  
+sayMe 3 = "Three!"  
+sayMe 4 = "Four!"  
+sayMe 5 = "Five!"  
+sayMe x = "Not between 1 and 5"  
