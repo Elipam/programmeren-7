35fdff813b147a254339afdcd5c62414e0cabaef
eliam <eliamtraas@gmail.com>
2023-02-15 17:05:17 +0100

    2a en 2b

--- opdracht_2.hs
+++ opdracht_2.hs
@@ -1,18 +1,14 @@
 -- 2a
-{-nulpuntena :: Double->Double->Double->[Double]
-nupuntena a b c
-    |d < 0 = []
-    |d == 0 = [-b/(2 * a)]
-    |d > 0 = [(-b + sqrt d) / (2* a), (-b - sqrt d) / (2* a)]
-    d = b * b - 4 * a * c  -}
-
 nulpuntena :: Double->Double->Double->[Double]  
-nulpuntena a b c  
+nulpuntena a b c = [(-b + sqrt (b * b - 4 * a * c))/ (2 * a), (-b - sqrt (b * b - 4 * a * c))/ (2 * a)]
+
+-- 2b
+nulpuntenb :: Double->Double->Double->[Double]  
+nulpuntenb a b c  
     | d < 0 = []  
     | d == 0 = [-b / (2 * a)] 
     | d > 0 = [(-b + sqrt d)/ (2 * a), (-b - sqrt d)/ (2 * a)]
     where d = b * b - 4 * a * c
--- 2b
 
 -- 2c
 
