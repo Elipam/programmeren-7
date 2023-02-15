75b4b8ec5c017a73d711875e7c4c046fa53d51d6
eliam <eliamtraas@gmail.com>
2023-02-15 15:29:30 +0100

    ouleh

--- opdracht_2.hs
+++ opdracht_2.hs
@@ -1,15 +1,20 @@
 -- 2a
-nulpuntena :: Double->Double->Double->[Double]
+{-nulpuntena :: Double->Double->Double->[Double]
 nupuntena a b c
     |d < 0 = []
     |d == 0 = [-b/(2 * a)]
     |d > 0 = [(-b + sqrt d) / (2* a), (-b - sqrt d) / (2* a)]
-    let d = b * b - 4 * a * c  
+    d = b * b - 4 * a * c  -}
 
+nulpuntena :: Double->Double->Double->[Double]  
+nulpuntena a b c  
+    | d < 0 = []  
+    | d == 0 = [1,2] 
+    | d > 0 = [1,2,3,d]
+    where d = b * b - 4 * a * c
 -- 2b
 
 -- 2c
 
 -- 2d
-main  do
-    print (nulpuntena(2,3,4))
+    
