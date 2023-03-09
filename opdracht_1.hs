83b58b28ad24656f97a804b386f949a8cebb732e
Eliam Traas <eliamtraas@gmail.com>
2023-03-09 14:23:23 +0100

    een paar enters toegevoegd

--- opdracht_1.hs
+++ opdracht_1.hs
@@ -10,6 +10,8 @@ facb :: Integer -> Integer
 facb n | n == 0 = 1 
        | n /= 0 = n * facb (n-1) 
 
+
+
 -- 2a
 nulpuntena :: Double->Double->Double->[Double]  
 nulpuntena a b c = [(-b + sqrt (b * b - 4 * a * c))/ (2 * a), (-b - sqrt (b * b - 4 * a * c))/ (2 * a)]
@@ -30,9 +32,13 @@ dobbelstenen = [ (a,b,c) | a <- [1..6], b <- [1..6], c <- [1..6],   (a + b + c) 
 dobbelstenen2 :: Integral c => c -> [(c, c, c)]
 dobbelstenen2 n = [ (a,b,c) | a <- [1..6], b <- [1..6], c <- [1..6], (a + b + c) `mod` n == 0 ]
 
+
+
 -- 3
 opg3 :: [(Integer, Integer, Integer)]
 opg3 = [(a, b, c)|a<-[-100..100],b<-[-100..100],c<-[-100..100],a==2*(b-c),b==a*c,c*2==a+b]
+
+
 
 -- 4a
 mult :: Integer->Integer->Integer 
@@ -45,6 +51,7 @@ De functie mult geeft een stackoverflow bij mult 10000000 10000000.
 1 nul minder kan nog wel, dan kan je ook zoveel cijfers toevoegen als je wel.
 Het gaat erom dat als beide values boven 7 cijfers hebben dan geeft het programma een stackoverflow. 
 -}
+
 -- 4b
 fastmult :: Integer->Integer->Integer
 fastmult a b
@@ -59,17 +66,17 @@ fastmult a b
 -- Als het kleinste bit van b 1 is, gooit de functie a in de opslag erbij. 
 -- Als laatste leest de functie de opslag wat het product van a en b houd
 
+
+
 -- 5a
 pow :: Integer -> Integer -> Integer
 pow x p
     | p <= 0 = 1
     | otherwise = x * pow x (p - 1)
-
   {- 
   pow 1 9100000 haalde het programma nog wel 
   pow 1 9200000 leverde een stack overflow op
   -}
-
 
 -- 5b
 fastpow :: Integer -> Integer -> Integer
@@ -77,7 +84,6 @@ fastpow x 0 = 1
 fastpow x p
     | even p    = fastpow (x * x) (shiftR p 1)
     | otherwise = x * fastpow (x * x) (shiftR p 1)
-
 {-
 Het duurt een tijdje maar het programma kan uiteindelijk wel de values die de mult functie een stackoverflow geven aan
 -}
