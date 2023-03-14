14fc6ce01d184b28b61b5453b91a8add8d0a4182
eliam <eliamtraas@gmail.com>
2023-03-14 14:59:42 +0100

    1b

--- opdracht_2.hs
+++ opdracht_2.hs
@@ -3,7 +3,14 @@ euclid :: Integer -> Integer -> Integer
 euclid x y
   | y == 0    = x
   | otherwise = euclid y (x `mod` y)
+
 -- 1b
+egcd :: Integer -> Integer -> (Integer,Integer,Integer)
+egcd 0 b = (b, 0, 1)
+egcd a b =
+    let (g, s, t) = egcd (mod b a) a
+    in (g, t - div b a * s, s)
+--
 -- 2
 --3a
 --3b
