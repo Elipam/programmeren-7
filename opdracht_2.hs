8c543858d48eddbaa1134f7973dcd0af1a42be01
Eliam Traas <eliamtraas@gmail.com>
2023-04-18 11:16:31 +0200

    nieuw

--- opdracht_2.hs
+++ opdracht_2.hs
@@ -1,6 +1,3 @@
-import Text.XHtml (base)
-import Distribution.ModuleName (main)
-import Data.Sequence.Internal.Sorting (popMinQ)
 -- 1a
 euclid :: Integer -> Integer -> Integer
 euclid x y
@@ -12,40 +9,11 @@ egcd :: Integer -> Integer -> (Integer,Integer,Integer)
 egcd 0 b = (b, 0, 1)
 egcd a b =
     let (g, s, t) = egcd (mod b a) a
-<<<<<<< HEAD
     in (g, t - (div b a) * s, s)
---
-=======
-    in (neg g modulo, neg(t- div b a * s)modulo, neg s modulo)
-    where modulo = mod a b
-
-neg :: Integer -> Integer -> Integer
-neg x m
-  | x < 0 = x + m
-  | otherwise = x
-
->>>>>>> origin/main
 -- 2
-modulus::Integer -> Integer -> Integer
-modulus p q = p*q
-
-modulusAccent::Integer -> Integer -> Integer
-modulusAccent p q = (p-1)*(q-1)
-
-privateKey:: Integer -> Integer
-privateKey ma = head [a |a<-[100..ma-1], euclid a ma == 1]
-
-publicKey:: Integer -> Integer -> Integer
-publicKey e ma = head [d | d <- [100..], (e * d) mod ma == 1]
-
-generateKey::Integer ->Integer -> (Integer, Integer, Integer)
-generateKey p q = (e,d,m)
-  where
-    ma = modulusAccent p q
-    e = privateKey ma
-    d = publicKey e ma
-    m = modulus p q
-
+reverse' :: [a] -> [a]  
+reverse' [] = []  
+reverse' (x:xs) = reverse' xs ++ [x] 
 --3a
 --3b
 -- 4
