c9bbd7059f9adee950e893a5f83c5c3a8969de91
hiddejdani <hiddejan.daniels@gmail.com>
2023-04-25 13:40:17 +0200

    3.1

--- opdracht_3.hs
+++ opdracht_3.hs
@@ -1,4 +1,12 @@
 -- 1a
+differentieer :: (Double -> Double) -> Double -> Double -> Double
+differentieer f p x = (f (x + p) - f(x - p)) / (2 * p)
+
 -- 1b
+
+
 -- 2
+dubbelen :: [a] -> [a]
+dubbelen s
+
 -- 3
