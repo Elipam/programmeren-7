2867c8c8f627a26cde91c855af91dfe9f5c01a15
Elipam <eliamtraas@gmail.com>
2023-06-05 13:09:22 +0200

    2 af

--- opdracht_3.hs
+++ opdracht_3.hs
@@ -1,12 +1,17 @@
+import Data.List
+
 -- 1a
 differentieer :: (Double -> Double) -> Double -> Double -> Double
-differentieer f p x = (f (x + p) - f(x - p)) / (2 * p)
+differentieer f p x = (f (x + p) - f x) / p
 
 -- 1b
-
+integreer :: (Double -> Double) -> Double -> Double -> Double -> Double
+integreer f a b p = sum [f (a + (b - a)) * (b - a)]
 
 -- 2
-dubbelen :: [a] -> [a]
-dubbelen s
+dubbelen :: Eq a => [a] -> [a]
+dubbelen s = nub [x | x <- s, count x > 1]
+  where
+    count x = length (filter (== x) s)
 
 -- 3
