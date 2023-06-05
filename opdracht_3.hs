479ec29345df6ded36b3449c816f2010161adc8b
Elipam <eliamtraas@gmail.com>
2023-06-05 15:43:05 +0200

    wat ik tot nu toe heb

--- opdracht_3.hs
+++ opdracht_3.hs
@@ -15,3 +15,26 @@ dubbelen s = nub [x | x <- s, count x > 1]
     count x = length (filter (== x) s)
 
 -- 3
+faca :: Float -> Float
+faca 0 = 1
+faca n = n * faca ( n - 1 )
+
+permutaties :: Float
+permutaties = faca 5
+
+permutatiesGelijk :: Float -> Float
+permutatiesGelijk x = permutaties / faca x
+
+select :: [Float] -> String
+select [a] 
+    | aantal == 5 = "Straight or Bust"
+    | aantal == 4 = "One pair"
+    | aantal == 3 = "Two pair or Three of a kind"
+    | aantal == 2 = "Full house or Four of a kind"
+    | aantal == 1 = "Poker"
+    where aantal = length(group [a])
+
+select2 :: [Float] -> String
+select2 [a]
+    |
+    where
