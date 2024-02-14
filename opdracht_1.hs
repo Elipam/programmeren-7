22cb6f649d7d64a8a45d520a8768f7403f847962
Eliam Traas <eliamtraas@gmail.com>
2024-02-14 21:57:57 +0100

    comments

--- opdracht_1.hs
+++ opdracht_1.hs
@@ -1,23 +1,28 @@
 import Data.Bits (Bits (shiftL, shiftR))
 
 -- 1a
+-- Berekent de faculteit dmv pattern matching
 faca :: Integer -> Integer
 faca 0 = 1
 faca x = x * faca (x - 1)
 
 -- 1b
+-- Berekent de faculteit dmv guards
 facb :: Integer -> Integer
 facb x
   | x == 0 = 1
   | x > 0 = x * facb (x - 1)
 
 -- 2a
+-- Berekent de nulpunten van tweedegraadsfunctie mbv where functie
 nulpuntena :: Double -> Double -> Double -> [Double]
-nulpuntena a b c = 
-    [(-b + discriminant) / (2 * a), (-b - discriminant) / (2 * a)]
-    where discriminant = sqrt (b * b - 4 * a * c)
+nulpuntena a b c =
+  [(-b + discriminant) / (2 * a), (-b - discriminant) / (2 * a)]
+  where
+    discriminant = sqrt (b * b - 4 * a * c)
 
 -- 2b
+-- Berekent de nulpunten van tweedegraadsfunctie dmv guards en where keyword
 nulpuntenb :: Double -> Double -> Double -> [Double]
 nulpuntenb a b c
   | d < 0 = []
@@ -27,28 +32,32 @@ nulpuntenb a b c
     d = b * b - 4 * a * c
 
 -- 2c
+-- Gooit dobbelsteen a, b en c en geeft de combinaties die opgeteld een veelvoud van 5 zijn terug, door som door 5 te delen en modulus te checken
 dobbelstenen :: [(Integer, Integer, Integer)]
 dobbelstenen = [(a, b, c) | a <- [1 .. 6], b <- [1 .. 6], c <- [1 .. 6], (a + b + c) `mod` 5 == 0]
 
 -- 2d
-dobbelstenen2 :: (Integral c) => c -> [(c, c, c)]
+-- Zelfde functie, maar nu kan je zelf de deler bepalen
+dobbelstenen2 :: Integer -> [(Integer, Integer, Integer)]
 dobbelstenen2 n = [(a, b, c) | a <- [1 .. 6], b <- [1 .. 6], c <- [1 .. 6], (a + b + c) `mod` n == 0]
 
 -- 3
+-- Maakt permutaties met alle getalen tussen de -100 en 100, stelt daarna de voorwaarden
 opg3 :: [(Integer, Integer, Integer)]
 opg3 = [(a, b, c) | a <- [-100 .. 100], b <- [-100 .. 100], c <- [-100 .. 100], a == 2 * (b - c), b == a * c, c * 2 == a + b]
 
 -- 4a
+-- Vermenigvuldigt dmv optellen
 mult :: Integer -> Integer -> Integer
 mult a b
   | b <= 0 = 0
   | otherwise = a + mult (b - 1) a
 
 {-
-De functie mult geeft een stackoverflow bij mult 10000000 10000000.
-1 nul minder kan nog wel, dan kan je ook zoveel cijfers toevoegen als je wel.
-Het gaat erom dat als beide values boven 7 cijfers hebben dan geeft het programma een stackoverflow.
+De functie mult geeft een stackoverflow bij mult 1000000000000 4500000.
+Zo lang een van de twee cijfers 45????? of daaronder blijft is er geen stackoverflow.
 -}
+
 -- 4b
 fastmult :: Integer -> Integer -> Integer
 fastmult a b
